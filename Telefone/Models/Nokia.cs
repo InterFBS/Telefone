60cec658129438cafdfacd9cac85c018f75efb54
FelipeBarroso <felipebarroso299@gmail.com>
2023-08-15 21:36:09 -0300

    Projeto do TELEFONE DESAFIO DIO

---
+++ Telefone/Models/Nokia.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telefone.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base (numero, modelo, imei, memoria)
+        {
+        }
+        
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine("Instalando o " + nomeApp);
+        }
+    }
+}
---
+++ Telefone/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+
+using Telefone.Models;
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("898378", "Iphone", "2978409283", 12);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("898378", "Iphone", "2978409283", 12);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
